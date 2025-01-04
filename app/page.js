f9795d7a0befd02e8d012f0aae850ce0f0a7589f
Swapnaneel Ray <neel26ray@gmail.com>
2025-01-05 01:36:31 +0530

    init

--- app/page.js
+++ app/page.js
@@ -1,101 +1,73 @@
-import Image from "next/image";
-
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
-}
+"use client"
+import React, { useState} from 'react'
+ 
+ const page = () => {
+   const [title, settitle] = useState("");
+   const [description, setdescription] = useState("")
+   const [task, settask] = useState([])
+   const submitHandler = (e) => {
+     e.preventDefault();
+     const obj = {
+       title: title,
+       description: description
+     };
+     settask([ ...task, obj ]);
+     console.log(title + " " + description);
+     setdescription("");
+     settitle("");
+   }
+   const deleteHandler = (i) => {
+     let copytask = [...task];
+     copytask.splice(i, 1);
+     settask(copytask)
+   }
+   return (
+     <div className=' h-screen items-center  flex flex-col w-screen bg-zinc-900'>
+      <HeaderComp/>
+       <form
+        onSubmit={submitHandler}>
+             <input type="text" className=' border-yellow-400 border-2 bg-inherit m-10 px-10 py-2'
+               placeholder='Enter Task'
+                value={title}
+               onChange={(e) => settitle(e.target.value)}></input>
+             
+             <input type="text" className=' border-yellow-400 border-2 bg-inherit m-10 px-10 py-2'
+                value={description}
+                 
+               placeholder='Enter Description'
+             onChange={(e)=>setdescription(e.target.value)}></input>
+             
+             <button className='bg-yellow-400 px-4 py-3 text-zinc-100 font-semibold  rounded'>Add Task</button>
+      </form>
+     <RenderTasks/>
+     </div>
+     
+   )
+   function HeaderComp() {
+     return(
+      <h1 className='text-7xl p-6 font-semibold text-center text-yellow-400' >
+         TodoList 🔥
+       </h1>)
+   }
+  
+    function RenderTasks() {
+      return <div className='flex flex-col  overflow-auto gap-4  justify-between'>
+      {
+        task.map((e,i) => {
+          return (
+                   <div key = {i} className='justify-between gap-10 flex flex-row'>
+                   <h1 className='text-3xl font-semibold text-white'>
+                     {  e.title}
+                   </h1>
+                   <h2 className='text-3xl font-semibold text-yellow-300'>
+                       {e.description}
+                     </h2>
+                     <button onClick={()=>deleteHandler(i)} className=' bg-red-500 rounded px-3 py-1'>Delete</button>
+                 </div>)
+               })
+             }
+           </div>
+    }
+ }
+ 
+ export default page
